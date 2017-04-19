7db02f6498d09349eaf75e0c14c2b0908305e6c2
kununcle6 <512947326@qq.com>
2017-04-19 15:01:41 +0800

    creat stu

--- Chat/app/src/main/java/com/example/administrator/ychat/MainActivity.java
+++ Chat/app/src/main/java/com/example/administrator/ychat/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.administrator.ychat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG ="asauaau";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e(TAG);
     }
 }
